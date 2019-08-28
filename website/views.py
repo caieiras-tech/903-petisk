3b02ccbc69cf6238725ae4ffd457353984633a92
Gabriel Roger <gabriel.nroger@gmail.com>
2019-08-28 17:32:57 -0300

    criar pessoa na base

--- website/views.py
+++ website/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
+from website.models import Pessoa
 
 # Create your views here.
 
 def index(request):
+
+
+    if request.method == 'POST':
+        pessoa = Pessoa()
+        pessoa.nome = request.POST.get('nome')
+        pessoa.sobrenome = request.POST.get('sobrenome')
+        pessoa.data_nascimento = request.POST.get('data_nascimento')
+        pessoa.email = request.POST.get('email')
+        pessoa.str_cep = request.POST.get('str_cep')
+        pessoa.str_numero = request.POST.get('str_numero')
+        pessoa.complemento = request.POST.get('complemento')
+        pessoa.genero = request.POST.get('genero')
+        pessoa.telefone = request.POST.get('telefone')
+        pessoa.celular = request.POST.get('celular')
+        pessoa.motivo = request.POST.get('motivo')
+        pessoa.save()
+
+        contexto = {
+            'nome': pessoa.nome
+        }
+        return render(request, 'index.html', contexto)
+
+
+    
     return render(request, 'index.html')
